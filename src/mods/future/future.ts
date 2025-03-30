c469e46eb09b10e749a97ac5fad891abeea5e29e
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-03-30 13:01:54 +0700

    update

--- src/mods/future/future.ts
+++ src/mods/future/future.ts
@@ -1,26 +1,42 @@
 export class Future<T> {
 
-  #resolve?: (value: T) => void
-  #reject?: (error: unknown) => void
+  #resolve: (value: T) => void
+
+  #reject: (error: unknown) => void
 
   readonly promise: Promise<T>
 
   /**
-   * Just like a Promise but you can manually resolve or reject it
+   * Just a Promise with a resolve and reject function
    */
-  constructor() {
-    this.promise = new Promise((subresolve, subreject) => {
-      this.#resolve = subresolve
-      this.#reject = subreject
-    })
+  constructor(promise?: Promise<T>) {
+    if (promise == null) {
+      const { promise, resolve, reject } = Promise.withResolvers<T>()
+
+      this.promise = promise
+      this.#resolve = resolve
+      this.#reject = reject
+    } else {
+      this.promise = promise
+      this.#resolve = () => { }
+      this.#reject = () => { }
+    }
+  }
+
+  static resolve<T>(value: T): Future<T> {
+    return new Future<T>(Promise.resolve<T>(value))
+  }
+
+  static reject<T>(error: unknown): Future<T> {
+    return new Future<T>(Promise.reject<T>(error))
   }
 
   get resolve() {
-    return this.#resolve!
+    return this.#resolve
   }
 
   get reject() {
-    return this.#reject!
+    return this.#reject
   }
 
 }
